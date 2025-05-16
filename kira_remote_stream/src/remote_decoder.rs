73870d4cce32dd6eec974e15dd023fed61243811
fragsalat <t.schlage@gmx.net>
2025-05-16 22:07:34 +0200

    remote_stream: Added retry mechanism

--- kira_remote_stream/src/remote_decoder.rs
+++ kira_remote_stream/src/remote_decoder.rs
@@ -5,14 +5,31 @@ pub struct RemoteStreamDecoder {}
 
 impl RemoteStreamDecoder {
     pub async fn from_url(url: String) -> Result<SymphoniaDecoder, String> {
-        let stream = RemoteMediaSource::from_url(url)
-            .await
-            .map_err(|error| format!("Could not create remote media source: {}", error))?;
+        let stream = Self::try_create_stream(url.clone()).await?;
         let decoder = tokio::task::spawn_blocking(move || {
             SymphoniaDecoder::new(Box::new(stream))
                 .map_err(|error| format!("Could not create remote decoder: {}", error))
-        }).await.unwrap()?;
+        })
+        .await
+        .unwrap()?;
 
         Ok(decoder)
+    }
+
+    async fn try_create_stream(url: String) -> Result<RemoteMediaSource, String> {
+        for attempt in 0..3 {
+            match RemoteMediaSource::from_url(url.clone()).await {
+                Ok(stream) => return Ok(stream),
+                Err(error) => {
+                    if attempt == 2 {
+                        return Err(format!(
+                            "Failed to create stream after 3 attempts: {}",
+                            error
+                        ));
+                    }
+                }
+            }
+        }
+        Err("Failed to create stream, some coding error".to_string())
     }
 }
